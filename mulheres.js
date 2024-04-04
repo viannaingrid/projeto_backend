007d92c6764e1772a11636ac3e616ed131821d62
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-04-04 20:46:57 -0300

    criando post /mulheres com o deply pelo render

--- mulheres.js
+++ mulheres.js
@@ -1,11 +1,14 @@
-const express = require("express") 
-const router = express.Router()
+const express = require("express")   //aqui estou iniciando o express
+const router = express.Router()  //aqui estou configurando a primeira parte da rota
+const { v4: uuidv4 } = require('uuid') //solicitando a biblioteca NPM uuid
 
-const app = express()    
-const porta = 3333
+const app = express()    //aqui estou iniciando o app
+const porta = 3333    //criando a porta 3333
 
+//Criado a lista inicial de mulheres 
 const mulheres = [
     {
+        id: '1',
 
         nome: 'Simara Conceição',
      
@@ -16,7 +19,8 @@ const mulheres = [
       },
      
       {
-     
+        id: '2',
+
         nome: 'Iana Chan',
      
         imagem: 'https://bit.ly/3JCXBqP',
@@ -26,6 +30,7 @@ const mulheres = [
       },
      
       {
+        id: '3',
      
         nome: 'Luana Pimentel',
      
@@ -36,13 +41,33 @@ const mulheres = [
       }
 ]
 
+//GET
 function mostraMulheres(request, response) {
     response.json(mulheres)
 }
 
+//POST 
+function criaMulher(request, response) {
+    const novaMulher = {
+      id: uuidv4(),
+      nome: request.body.nome,
+      imagem: request.body.imagem,
+      minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+    
+}
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+
+ 
+app.use(router.get('/mulheres', mostraMulheres)) //configurado rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurado rota POST /mulheres
+app.listen(porta, mostraPorta)  //Servidor ouvindo a porta
